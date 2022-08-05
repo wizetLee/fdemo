e30658fa6533e9a410f62bf33585cc2c93ad04d2
wizet <wizet@wizetdeMac-Studio.local>
2022-08-05 17:11:44 +0800

    update

--- lib/system_lib_test/system_lib_test.dart
+++ lib/system_lib_test/system_lib_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:device_info/device_info.dart';
 
 
 class SystemLibTestRoute extends StatefulWidget {
@@ -18,26 +19,37 @@ class _SystemLibTestRouteState extends State<SystemLibTestRoute> {
     // List<int> list = [1, 2, 3, 4, 5];
     // var target = list.sublist(1, 10);
 
-    Map<String, dynamic> json = {
-    "stock_list": ["SZ301002","SZ000591","SH603109","SH688345","SZ300582","SZ000931","SZ300988","SZ002931","SH688616","SZ001696"]};
-    var stockList = json['stock_list'].cast<String>();
-    print("object = ${stockList}");
-    convertTest();
-
-    var c = "IX9074326";
-    var c1 = "IX907";
-
-    if (c.contains(c1)) {
-      print("gou");
-    } else {
-      print("x");
-    }
-
-
+    // Map<String, dynamic> json = {
+    // "stock_list": ["SZ301002","SZ000591","SH603109","SH688345","SZ300582","SZ000931","SZ300988","SZ002931","SH688616","SZ001696"]};
+    // var stockList = json['stock_list'].cast<String>();
+    // print("object = ${stockList}");
+    // convertTest();
+    //
+    // var c = "IX9074326";
+    // var c1 = "IX907";
+    //
+    // if (c.contains(c1)) {
+    //   print("gou");
+    // } else {
+    //   print("x");
+    // }
+    //
+    //
     var p = Platform();
-    print(Platform.environment);
-    print(Platform.operatingSystem);
-    print(Platform.operatingSystemVersion);
+    print("Platform.environment = ${Platform.environment}");
+    print("Platform.operatingSystem = ${Platform.operatingSystem}");
+    print("Platform.operatingSystemVersion = ${Platform.operatingSystemVersion}");
+    print("Platform.version = ${Platform.version}");
+    print("Platform.packageConfig = ${Platform.packageConfig}");
+    print("Platform.executableArguments = ${Platform.executableArguments}");
+    print("Platform.script = ${Platform.script}");
+    print("Platform.resolvedExecutable = ${Platform.resolvedExecutable}");
+    // print("Platform.executable = ${Platform.executable}");
+    print("Platform.environment = ${Platform.environment}");
+    print("Platform.localHostname = ${Platform.localHostname}");
+    print("Platform.localeName = ${Platform.localeName}");
+    print("Platform.pathSeparator = ${Platform.pathSeparator}");
+    print("Platform.numberOfProcessors = ${Platform.numberOfProcessors}");
 
     return Container(color: Colors.orange,);
   }
